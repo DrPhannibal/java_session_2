8d9a2efe5bfac78bf277342c75dc03cd327448cc
DrPhannibal <mahdisouri0@gmail.com>
2022-04-13 00:07:02 +0430

    java_session_2

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int number_1,number_2=30;
+        number_1=20;
+        ////print numbers in console ////
+        System.out.println(number_1+" "+number_2);
+    }
+}
